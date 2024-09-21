6b1aeec35364f1c950d9257f5b8c40bb2a1347fe
Shrey <shreyshukla1010@gmail.com>
2024-09-21 16:55:53 +0530

    adding list component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ export default function App(){
 
 }
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 function Logo(){
-  return <h1>🌴 Far Away 🎒</h1>
+  return <h1>🌴 Far Away ✈️</h1>
 }
 
 function Form(){
@@ -19,7 +23,24 @@ function Form(){
   </div>
 }
 function PackingList(){
-  return <div className="list">List</div>
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({item}){
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration:"line-through"}: {}}>{item.quantity} {item.description}</span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats(){
   return <footer className="stats"><em>You have X item on your list, and you already packed X (X%)</em></footer>
